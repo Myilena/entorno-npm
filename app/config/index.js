05499c86b5a8d14a10333e1bf18e3ac1d77d6a95
ddnarvaez <dandnarvaez@misena.edu.co>
2022-11-01 17:36:43 -0500

    TRABAJO CLIMA YILENA

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,26 @@
-const path = require("path"); // busca el archivo .env, config tiene unos parametros obsionales
-let ruta = `enviroments/${process.env.ENTORNO}.env`;
-console.log(process.env.ENTORNO);
-const dotenv = require("dotenv").config({
-  path: path.resolve(__dirname, ruta),
-});
+    const path = require("path"); // busca el archivo .env, config tiene unos parametros obsionales
+    let ruta = `enviroments/${process.env.ENTORNO}.env`;
+    console.log(process.env.ENTORNO);
+    const dotenv = require("dotenv").config({
+    path: path.resolve(__dirname, ruta),
+    });
+
+    module.exports = callback= ()=>{
+        mensaje = `Esta conectado en la url http://localhost:${process.env.port}`;
+        switch (process.env.ENTORNO) {
+        case "qa":
+            console.log(mensaje.bgYellow);
+    
+            break;
+        case "development":
+            console.log(mensaje.bgBlue);
+            break;
+        case "production":
+            console.log(mensaje.bgRed);
+            break;
+    
+        default:
+            console.log(mensaje.rainbow);
+            break;
+        } //Este se encarga de darle el color segun el trabajo que estemos haciendo//
+    }
--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-const express = require("express");
-const colors = require("colors");
-const config = require("./app/config");
+    const express = require("express");
+    const colors = require("colors");
+    const config = require("./app/config");
 
-const server = express();
+    const server = express();
 
-const port = process.env.port;
-//const port = 3000;
+    const port = process.env.port;
+    //const port = 3000;
 
-server.get("/", (req, res) => {
-  res.send("hola mundo");
-});
+    server.get("/", (req, res) => {
+    res.send("hola mundo");
+    });
 
-server.listen(port, () => {
-  mensaje = `Esta conectado en la url http://localhost:${port}`;
-  switch (process.env.ENTORNO) {
-    case "qa":
-      console.log(mensaje.bgYellow);
-
-      break;
-    case "development":
-      console.log(mensaje.bgBlue);
-      break;
-    case "production":
-      console.log(mensaje.bgRed);
-      break;
-
-    default:
-      console.log(mensaje.rainbow);
-      break;
-  } //Este se encarga de darle el color segun el trabajo que estemos haciendo//
-});
+    server.listen(port,callback);
